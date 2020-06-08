4329b28e6a82bc0a0372dfc3dcc49c8e14ca9344
naztuo <1249482132@qq.com>
2020-06-08 17:28:59 +0800

    第二次提交

--- microcloud-consumer-feign/src/main/java/cn/enjoy/ConsumerFeignApp.java
+++ microcloud-consumer-feign/src/main/java/cn/enjoy/ConsumerFeignApp.java
@@ -3,15 +3,16 @@ package cn.enjoy;
 import cn.xiangxue.config.RibbonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name ="MICROCLOUD-PROVIDER-PRODUCT" ,configuration = RibbonConfig.class)
-public class ConsumerApp {
+@EnableFeignClients("cn.enjoy.service")
+//@RibbonClient(name ="MICROCLOUD-PROVIDER-PRODUCT" ,configuration = RibbonConfig.class)
+public class ConsumerFeignApp {
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApp.class,args);
+        SpringApplication.run(ConsumerFeignApp.class,args);
     }
 }
