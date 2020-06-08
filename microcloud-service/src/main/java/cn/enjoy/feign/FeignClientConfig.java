fc2d372db50872062fa9227c2faa9fd21bd1a96e
naztuo <1249482132@qq.com>
2020-06-08 17:39:35 +0800

    第二次提交

--- microcloud-service/src/main/java/cn/enjoy/feign/FeignClientConfig.java
+++ microcloud-service/src/main/java/cn/enjoy/feign/FeignClientConfig.java
@@ -1,4 +1,5 @@
 package cn.enjoy.feign;
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,4 +10,10 @@ public class FeignClientConfig {
     public BasicAuthRequestInterceptor getBasicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor("admin", "enjoy");
     }
+
+    @Bean
+    public Logger.Level getFeignLoggerLevel() {
+        return feign.Logger.Level.FULL ;
+    }
+
 }
